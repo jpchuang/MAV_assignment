8001c9ae14c811ab54549bb86ad96553338cda0f
j.p.huang@student.tudelft.nl <j.p.huang@student.tudelft.nl>
2020-04-26 00:27:33 +0200

    addition of a SURF detection method and figures

--- SURF.py
+++ SURF.py
@@ -1,4 +1,4 @@
-# Gate detection method using SIFT - matching to a gate template image
+# Gate detection method using SURF
 
 # modules
 import numpy as np
@@ -16,21 +16,43 @@ min_matching = 15  # minimum number of keypoint areas
 
 # Read the matching template and the testing image in grayscale
 img_template = cv2.imread("matching_template_3.png", cv2.IMREAD_GRAYSCALE)
+# resize the template image
 img_template = cv2.resize(img_template, (template_size, template_size))
 img_test = cv2.imread("test_data.png", cv2.IMREAD_GRAYSCALE)
 
-# Initiate SIFT Detector
-sift = cv2.xfeatures2d.SIFT_create()
+# Addition of test images to check scale and rotational invariance
+img_test_invariance = cv2.pyrDown(img_test)
+nrows, ncols = img_test_invariance.shape[:2]
 
-# find the keypoints and descriptors with SIFT
-kp_template, des_template = sift.detectAndCompute(img_template, None)
-kp_test, des_test = sift.detectAndCompute(img_test, None)
+# rotation matrix 45 degrees and scale size of 1
+rotation_matrix = cv2.getRotationMatrix2D((ncols/2, nrows/2), 45, 1)
+# transformation of original image using rotation matrix
+img_test_invariance = cv2.warpAffine(
+    img_test_invariance, rotation_matrix, (ncols, nrows))
 
-# Same matching method as SURF
+img_test_invariance = cv2.resize(
+    img_test_invariance, (invariance_size, invariance_size))
+
+# display transformed image for testing purposes
+# plt.imshow(img_test_invariance), plt.show()
+
+# Initiate SURF detection method
+# larger value filters more extensively (500-1000 value)
+surf = cv2.xfeatures2d.SURF_create(800)
+
+# detection and extraction of feature keypoints + feature description (step 1 and 2 in the method explanation)
+kp_template, des_template = surf.detectAndCompute(img_template, None)
+kp_test, des_test = surf.detectAndCompute(img_test, None)
+
+# Next step is matching the detected keypoint regions using the FLANN Matcher method in OpenCV
+# usage of flann index tree algorithm which indexes the features in a tree like structure and iterates through it
 FLANN_INDEX_KDTREE = 0
+# Pass of two dictionaries that specifies the algorithm that is going to be used and its related parameters
 index_dict = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+# this specifies the number of times the trees in the index dict should be recursively traversed
 search_dict = dict(checks=50)
 
+# initiate matching using the extracted descriptor vectors
 flann = cv2. FlannBasedMatcher(index_dict, search_dict)
 matches = flann.knnMatch(des_template, des_test, k=2)
 
@@ -40,7 +62,9 @@ for m, n in matches:
     if m.distance < 0.75 * n.distance:  # threshold of less than 0.75
         inliers.append(m)
 
+# Enables for a minimum amount of matches otherwise the detection method will not work properly
 if len(inliers) > min_matching:
+    # extract the locations of matched keypoints in both images
     img_template_kp = np.float32(
         [kp_template[m.queryIdx].pt for m in inliers]).reshape(-1, 1, 2)
     img_test_kp = np.float32(
@@ -53,12 +77,15 @@ if len(inliers) > min_matching:
     pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]
                      ).reshape(-1, 1, 2)
     dst = cv2.perspectiveTransform(pts, M)
+
+    # draw the lines around the detected object
     img_test = cv2.polylines(
         img_test, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
 else:
     print("Not enough matches are found")
     matchesMask = None
 
+# Draw the inlier matches that were found previously
 draw_matches = dict(matchColor=(0, 255, 0),  # draw matches in green color
                     singlePointColor=None,
                     matchesMask=matchesMask,  # draw only inliers
